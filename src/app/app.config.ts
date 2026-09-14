a7b7c8de78d1713d8782de7f3064fb805988d850
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2026-09-14 22:23:38 +0200

    Set up the ngxs store.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,10 +2,13 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { withNgxsReduxDevtoolsPlugin } from '@ngxs/devtools-plugin';
+import { provideStore } from '@ngxs/store';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    provideStore([], withNgxsReduxDevtoolsPlugin()),
+  ],
 };
