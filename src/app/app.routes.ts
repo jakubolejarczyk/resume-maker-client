2c9e4802c63ad7e3af6959d086d5c6195395b97e
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2026-09-14 18:31:29 +0200

    Create navigation routing tihe the most important routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,33 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { ShowUsersView } from '../view/show-users/show-users-view';
+import { ShowCompaniesView } from '../view/show-companies/show-companies-view';
+import { ShowResumesView } from '../view/show-resumes/show-resumes-view';
+
+export const routes: Routes = [
+    {
+        path: "",
+        redirectTo: "users",
+        pathMatch: "full"
+    },
+    {
+        path: "users",
+        title: "Users",
+        component: ShowUsersView
+    },
+    {
+        path: "companies",
+        title: "Companies",
+        component: ShowCompaniesView
+    },
+    {
+        path: "resumes",
+        title: "Resumes",
+        component: ShowResumesView
+    },
+    {
+        path: "**",
+        redirectTo: "users",
+        pathMatch: "full"
+    }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,24 +1,12 @@
 import { Component } from '@angular/core';
-import { ShowUsersView } from '../view/show-users/show-users-view';
-import { CreateUserView } from '../view/create-user/create-user-view';
-import { ShowCompaniesView } from '../view/show-companies/show-companies-view';
-import { CreateCompanyView } from '../view/create-company/create-company-view';
-import { ShowResumesView } from '../view/show-resumes/show-resumes-view';
-import { CreateResumeView } from '../view/create-resume/create-resume-view';
+import { RouterOutlet } from '@angular/router';
+
 import { NavComponent } from '../component/nav/nav-component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.scss',
-  imports: [
-    ShowUsersView,
-    CreateUserView,
-    ShowCompaniesView,
-    CreateCompanyView,
-    ShowResumesView,
-    CreateResumeView,
-    NavComponent
-  ]
+  imports: [NavComponent, RouterOutlet]
 })
 export class App {}
