df96c357153b8e92bc7874751b47d89d3272f14d
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2026-09-15 15:25:33 +0200

    Refactor the hamburger and menu

--- src/component/menu/menu.component.ts
+++ src/component/menu/menu.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject } from "@angular/core";
 import { Store } from "@ngxs/store";
 import { AsyncPipe } from "@angular/common";
+import { RouterLink } from "@angular/router";
 
 import { MenuStoreState } from "../../store/state/menu-store.state";
 import { SwitchMainMenuIsOpen } from "../../store/action/menu-store.action";
@@ -9,7 +10,7 @@ import { SwitchMainMenuIsOpen } from "../../store/action/menu-store.action";
     selector: "app-menu-component",
     templateUrl: "./menu.component.html",
     styleUrl: "./menu.component.scss",
-    imports: [AsyncPipe]
+    imports: [AsyncPipe, RouterLink]
 })
 export class MenuComponent {
     store = inject(Store);
