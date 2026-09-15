846748a175b5256200e277bb9cbf7c8aaa7f164e
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2026-09-15 16:05:29 +0200

    Remove the routing

--- src/config/routes.config.ts
+++ src/config/routes.config.ts
@@ -1,28 +1,3 @@
 import { Routes } from '@angular/router';
 
-import { ShowCompaniesView } from '../view/show-companies/show-companies-view';
-import { ShowResumesView } from '../view/show-resumes/show-resumes-view';
-import { ShowUsersView } from '../view/show-users/show-users-view';
-
-export const routesConfig: Routes = [
-    {
-        path: "users",
-        title: "Users",
-        component: ShowUsersView
-    },
-    {
-        path: "companies",
-        title: "Companies",
-        component: ShowCompaniesView
-    },
-    {
-        path: "resumes",
-        title: "Resumes",
-        component: ShowResumesView
-    },
-    {
-        path: "**",
-        redirectTo: "users",
-        pathMatch: "full"
-    }
-];
+export const routesConfig: Routes = [];
