84bf86dfe52037fdf81878f860f8a2a786c6a63d
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2026-09-17 15:11:10 +0200

    Add window directive to the app component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 
 import { NavComponent } from '../component/nav/nav.component';
+import { WindowDirective } from '../directive/window.directive';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.scss',
-  imports: [NavComponent]
+  imports: [NavComponent, WindowDirective]
 })
 export class App {}
