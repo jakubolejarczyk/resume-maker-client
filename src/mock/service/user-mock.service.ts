7d6a694fd299258e78a4b9d357e3a1ed8f1fc7d8
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2026-09-20 17:50:38 +0200

    Add summary to the users mock service

--- src/mock/service/user-mock.service.ts
+++ src/mock/service/user-mock.service.ts
@@ -18,7 +18,7 @@ export class UserMockService extends BaseMockService<UserMockModel> {
                 city: "Kielce",
                 country: "Poland",
                 links: [],
-                summary: ""
+                summary: "Senior Software Engineer with 6+ years of experience specializing in full-stack web development with Angular, TypeScript, C#, and ASP.NET Core. Experienced in designing and developing scalable frontend applications, RESTful APIs, and backend services, with a strong focus on clean architecture, performance, maintainability, and code quality. Proven ability to work across the full development lifecycle and collaborate effectively with cross-functional teams to deliver reliable software solutions."
             }
         ]);
     }
