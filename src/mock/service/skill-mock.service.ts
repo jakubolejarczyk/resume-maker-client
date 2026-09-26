70c511539df419fe0bb2c196eb646fddb27fc97b
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2026-09-26 14:43:23 +0200

    Add angular to the skills and languagers

--- src/mock/service/skill-mock.service.ts
+++ src/mock/service/skill-mock.service.ts
@@ -8,9 +8,20 @@ import { UUIDUtil } from "../../util/uuid.util";
 export class SkillMockService {
     private skills: SkillMockModel[] = [
         {
+            id: "abcd0574-a85e-4e7f-9d14-d4fc4067a027",
+            category: "Frontend",
+            skills: [
+                "Angular"
+            ],
+            userId: "10229cd3-5321-4692-9996-6d14d01558aa"
+        },
+        {
             id: "dd15a80e-7ea6-43da-a027-da762387ea2f",
-            category: "",
-            skills: [],
+            category: "Languages",
+            skills: [
+                "English",
+                "Polish"
+            ],
             userId: "10229cd3-5321-4692-9996-6d14d01558aa"
         }
     ];
